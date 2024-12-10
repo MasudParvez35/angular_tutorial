35d868b8831f110bec9c42f779d6e290097d7cc9
Masud Parvez <masud.parvez@brainstation.com>
2024-12-10 09:37:49 +0600

    crate components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_tutorial';
+  title = 'Angular Practice Project';
 }
