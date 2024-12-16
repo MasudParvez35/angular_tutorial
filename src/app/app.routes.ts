692e8a393630db057862cec8d4fbc3d3e402ad14
Masud Parvez <masud.parvez@brainstation.com>
2024-12-16 10:38:39 +0600

    fetch data from custom api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'add-emp',
+        component: AddEmployeeComponent
+    },
+    {
+        path: 'emp-list',
+        component: EmployeeListComponent
+    },
+    {
+        path: 'data-binding',
+        component: DataBindingComponent
+    }
+];
--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-employee-list',
-  imports: [],
+  imports: [HttpClientModule,CommonModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
+  students: any[] = [];
+  errorMessage: string = '';
 
+  private apiUrl = 'https://localhost:7155/api/Student/GetStudents';
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchStudents();
+  }
+
+  fetchStudents(): void {
+    this.getStudents().subscribe(
+      (data:any) => {
+        this.students = data;
+      },
+      (error:any) => {
+        this.errorMessage = 'Could not load students.';
+        console.error('Error fetching students:', error);
+      }
+    );
+  }
+  getStudents(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
